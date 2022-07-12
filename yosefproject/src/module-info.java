40899a6b35c1bafae0fbcffea937bb3f450842a9
yosef <108777322+bobilaw@users.noreply.github.com>
2022-07-12 18:30:36 -0400

    commmit from eclipse

---
+++ yosefproject/src/module-info.java
@@ -0,0 +1,2 @@
+module yosefproject {
+}
---
+++ yosefproject/src/yosefproject1/hello.java
@@ -0,0 +1,13 @@
+package yosefproject1;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		System.out.println("Greeting, From craft common 15");
+		
+		
+	}
+
+}
