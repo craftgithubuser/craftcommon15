e1fac263e3d75b9641d860e56eada18fe0c9ca6e
Tizita94 <tizitabebe12@gmail.com>
2022-07-12 10:20:32 -0700

    new commit

--- Samplejavaproject/src/com/git/Sample.java
+++ Samplejavaproject/src/com/git/Sample.java
@@ -5,6 +5,10 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println(" hello");
+       System.out.println(" hello there");
+       System.out.println(" thank you");
+
+
 	}
 
 }
