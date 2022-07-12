3d23ab6c516a7219845bd2700b2272692205c60d
tesgiz <tesgiz@hotmail.com>
2022-07-11 22:12:08 -0400

    ommit from Crafts

---
+++ JavaSampleProject/src/com/icraft/Test1.java
@@ -0,0 +1,14 @@
+package com.icraft;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Greetings: from Craft Common 15");
+		System.out.println("Thank you!!!!");
+
+	}
+
+}
