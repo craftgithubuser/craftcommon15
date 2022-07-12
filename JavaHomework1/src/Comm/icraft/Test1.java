049d765d3d933260427cd83a10b627abf4c92e23
Firehiwott <108773420+Firehiwott@users.noreply.github.com>
2022-07-12 06:25:32 -0600

    I make change

--- JavaHomework1/src/Comm/icraft/Test1.java
+++ JavaHomework1/src/Comm/icraft/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hey classmate");
+		System.out.print("Hey classmates");
 		System.out.print("Good morning everyone");
 		
 
--- JavaHomework1/src/Comm/icraft/Test2.java
+++ JavaHomework1/src/Comm/icraft/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("I feel good");
+		System.out.print("I feel good Ok");
 
 
 	}
