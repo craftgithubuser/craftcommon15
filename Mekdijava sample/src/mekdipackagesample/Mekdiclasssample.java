00dbbb53b16a5c2de2902635b69b8edf74a63556
MekdiA <nunuye21@gmail.com>
2022-07-12 13:29:53 -0400

    Merge branch 'main' of https://github.com/craftgithubuser/craftcommon15.git into main added text message

--- Mekdijava sample/src/mekdipackagesample/Mekdiclasssample.java
+++ Mekdijava sample/src/mekdipackagesample/Mekdiclasssample.java
@@ -5,6 +5,7 @@ public class Mekdiclasssample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		  System.out.println("selam world");
+		  System.out.println("selam world");
 	}
 
 }
