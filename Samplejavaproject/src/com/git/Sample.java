03225da010655a427390414256524fe3879afecb
Twordofa <tizitabebe12@gmail.com>
2022-07-12 11:20:27 -0700

    file is updated

--- Samplejavaproject/src/com/git/Sample.java
+++ Samplejavaproject/src/com/git/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println(" hello");
+       System.out.println(" hello world");
        System.out.println(" hello there");
        System.out.println(" thank you");
        System.out.println(" thank's");
