03c5b048cc500a1f2c43a287f1e07d9a860602de
zewdgit <108776569+zewdgit@users.noreply.github.com>
2022-07-11 22:32:03 -0400

    Eclipse commit

--- New_Sample_Java_Project15/src/Package15/Test15.java
+++ New_Sample_Java_Project15/src/Package15/Test15.java
@@ -8,7 +8,7 @@ System.out.println("Greeting, From CraftKnowlage15");
 System.out.println("Greeting, From CraftKnowlage15");
 System.out.println("Greeting, From CraftKnowlage15");
 System.out.println("Greeting, From CraftKnowlage15");
-System.out.println("Greeting, From CraftKnowlage15");
+
 
 	}
 
