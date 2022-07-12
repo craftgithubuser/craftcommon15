e91df677e558f1c8aeb51050807e4edd242971bf
Zinaye22 <108776620+Zinaye22@users.noreply.github.com>
2022-07-12 15:11:41 -0400

    I made some change

--- sampleproject2/src/samplepackage2/Class2.java
+++ sampleproject2/src/samplepackage2/Class2.java
@@ -5,7 +5,7 @@ public class Class2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		System.out.println("Greetings, all");
+		System.out.println("Happy Class all");
 		System.out.println("Thank you!!!");
 
 	}
