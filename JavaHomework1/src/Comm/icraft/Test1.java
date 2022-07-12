fd4a29d4f2e05457e9696d0f4f5762e42f017151
Firehiwott <108773420+Firehiwott@users.noreply.github.com>
2022-07-12 06:10:20 -0600

    Merge branch 'main' of https://github.com/craftgithubuser/craftcommon15.git into main

---
+++ JavaHomework1/src/Comm/icraft/Test1.java
@@ -0,0 +1,12 @@
+package Comm.icraft;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hey classmate");
+		System.out.print("Good morning everyone");
+
+	}
+
+}
