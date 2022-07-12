5624c49ee4b8d35c69c167587057d2850e66bc32
Twordofa <tizitabebe12@gmail.com>
2022-07-12 09:53:31 -0700

    commit

--- Samplejavaproject/src/gitsample/test2.java
+++ Samplejavaproject/src/gitsample/test2.java
@@ -4,7 +4,10 @@ public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      System.out.println("second class test2");
+      System.out.println("hello");
+      
+		
 	}
 
 }
