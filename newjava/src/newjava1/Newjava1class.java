4731878a2cbed09b8e246f1bf11dd2d8735afa92
AzariaMeheret <azariameheret@icloud.com>
2022-07-12 12:22:46 -0700

    Greeting from Azaria.

---
+++ newjava/src/newjava1/Newjava1class.java
@@ -0,0 +1,10 @@
+package newjava1;
+
+public class Newjava1class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("greetings, from Azaria");
+	}
+
+}
