e7c74aac3d52c624b3fc98a79908ed559814cf10
zewdgit <108776569+zewdgit@users.noreply.github.com>
2022-07-11 22:50:46 -0400

    commit

--- New_Sample_Java_Project15/src/Package15/Test15.java
+++ New_Sample_Java_Project15/src/Package15/Test15.java
@@ -5,8 +5,8 @@ public class Test15 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Greeting, From CraftKnowlage15");
-System.out.println("Greeting, From CraftKnowlage15");
-System.out.println("Greeting, From CraftKnowlage15");
+
+
 
 
 
