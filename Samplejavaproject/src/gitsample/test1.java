ffdb5fc44325d29dd59a2d00472b363c95af5415
Twordofa <tizitabebe12@gmail.com>
2022-07-12 11:12:47 -0700

    another update

--- Samplejavaproject/src/gitsample/test1.java
+++ Samplejavaproject/src/gitsample/test1.java
@@ -5,7 +5,7 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("this is my first exercise");
+		System.out.println("this is my first exercise!!!");
 		System.out.println("this is my first eclipse exercise");
 		
 	}
