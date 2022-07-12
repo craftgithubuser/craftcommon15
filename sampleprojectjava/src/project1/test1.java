04b0462170ce25a2f848ff23a8d45f5cf7d824a2
Zinaye22 <108776620+Zinaye22@users.noreply.github.com>
2022-07-12 13:06:36 -0400

    test1 is updated

--- sampleprojectjava/src/project1/test1.java
+++ sampleprojectjava/src/project1/test1.java
@@ -5,7 +5,7 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Greetings,");
+		System.out.println("Greetings, from me");
 		System.out.println("Thanks!!");
 		
 	}
