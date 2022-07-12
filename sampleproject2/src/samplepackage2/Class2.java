49182d267ee6a7601ab079abae4750c62acd0491
Zinaye22 <108776620+Zinaye22@users.noreply.github.com>
2022-07-12 15:04:14 -0400

    Merge branch 'main' of https://github.com/craftgithubuser/craftcommon15.git into main

--- sampleproject2/src/samplepackage2/Class2.java
+++ sampleproject2/src/samplepackage2/Class2.java
@@ -6,7 +6,7 @@ public class Class2 {
 		// TODO Auto-generated method stub
 	
 		System.out.println("Greetings, all");
-		System.out.println("Thank you");
+		System.out.println("Thank you everyone");
 
 	}
 }
