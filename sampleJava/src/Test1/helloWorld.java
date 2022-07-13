eba4aa0ad07a97b9bb2eeccdd9cb62258259f32c
abiy07 <abiy206@gmail.com>
2022-07-12 18:01:52 -0700

    2nd commit

--- sampleJava/src/Test1/helloWorld.java
+++ sampleJava/src/Test1/helloWorld.java
@@ -4,7 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 	}
 
 }
