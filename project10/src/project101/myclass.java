acae057a28fe675355c4b7ab96dc058d7518a18e
yosef <108777322+bobilaw@users.noreply.github.com>
2022-07-12 20:22:16 -0400

    commit from eclipse

---
+++ project10/src/project101/myclass.java
@@ -0,0 +1,12 @@
+package project101;
+
+public class myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Greetings, From craft commom 15");
+	}
+
+}
---
+++ project10/src/yp/sample2.java
@@ -0,0 +1,11 @@
+package yp;
+
+public class sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Greeting, From craft common 15");
+	}
+
+}
