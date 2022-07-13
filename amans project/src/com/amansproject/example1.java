5332678b1bf2a476de256bb718cb747919b02572
97tilinfinity <108904569+97tilinfinity@users.noreply.github.com>
2022-07-13 11:30:11 -0700

    commit from eclipse

---
+++ amans project/src/com/amansproject/example1.java
@@ -0,0 +1,17 @@
+package com.amansproject;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+
+	}
+
+}
