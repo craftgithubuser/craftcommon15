c556eff5bf64e6e33f0efa90a0f4096c4fdc9c7d
EmebetH <eminatye@gmail.com>
2022-07-12 22:12:36 -0400

    DBMS installation

--- first_Java_Project/src/Hellow_world.java
+++ first_Java_Project/src/Hellow_world.java
@@ -7,6 +7,7 @@ public class Hellow_world {
 		System.out.println("greetings");
 		System.out.println("Wellcome");
 		System.out.println("MySql server is on progress");
+		Syste.out.Println("database installation");
 		
 	}
 
