2006aedd2213e9312198a7493c825650bf731340
muluken <muluken@mulukens-mbp.attlocal.net>
2022-07-13 09:12:29 -0500

    this is my change

---
+++ ExampleJavaproject/src/craft/com/testone.java
@@ -0,0 +1,13 @@
+package craft.com;
+
+public class testone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.print("Greetings,from the craft common15");	
+System .out.print("Thanks!!!");
+	}
+	
+
+}
