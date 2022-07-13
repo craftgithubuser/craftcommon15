7c0921a728d99886488fdea5a75df7072b0754a6
MekdiA <nunuye21@gmail.com>
2022-07-12 21:02:09 -0400

    file added

--- Mekdijava sample/src/mekdipackagesample/Mekdiclasssample.java
+++ Mekdijava sample/src/mekdipackagesample/Mekdiclasssample.java
@@ -6,6 +6,7 @@ public class Mekdiclasssample {
 		// TODO Auto-generated method stub
 		  System.out.println("selam world");
 		  System.out.println("selam world");
+		  System.out.println("selam world");
 	}
 
 }
