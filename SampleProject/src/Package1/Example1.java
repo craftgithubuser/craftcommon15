c827a35e0d536e06662c36418c2670b680a96af5
tesgiz <tesgiz@hotmail.com>
2022-07-14 16:25:08 -0400

    commit 1

---
+++ SampleProject/src/Package1/Example1.java
@@ -0,0 +1,13 @@
+package Package1;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("This is a message from Eclipse for the creation of files to commit to GitHub");
+	System.out.println("thankYou!");
+
+	}
+
+}
