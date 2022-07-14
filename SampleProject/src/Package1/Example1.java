e37b6e6cf584b0c96163398eac3791260e053562
tesgiz <tesgiz@hotmail.com>
2022-07-14 16:25:42 -0400

    commit 2

--- SampleProject/src/Package1/Example1.java
+++ SampleProject/src/Package1/Example1.java
@@ -6,7 +6,7 @@ public class Example1 {
 		// TODO Auto-generated method stub
 		
 	System.out.println("This is a message from Eclipse for the creation of files to commit to GitHub");
-	System.out.println("thankYou!");
+	System.out.println("thankYou very much!");
 
 	}
 
