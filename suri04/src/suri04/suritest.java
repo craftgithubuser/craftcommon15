55d31a4818262ff55184049fdc2703707aa6e489
kibromkingo <108776587+kibromkingo@users.noreply.github.com>
2022-07-14 19:16:16 -0400

    i modified

--- suri04/src/suri04/suritest.java
+++ suri04/src/suri04/suritest.java
@@ -4,7 +4,8 @@ public class suritest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("i pass the exam");
+System.out.println("Thank you");
+
 	}
 
 }
