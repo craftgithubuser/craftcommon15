277a0684f1824b3636066e712eeececc192c5afe
amossie <addisumossie4@gmail.com>
2022-07-13 22:33:45 -0400

    I know this is not the right place to give this demo, it just practice.

--- iCraft_muleSoft_projct/src/iCraft_muleSoft_projct1/Sample1.java
+++ iCraft_muleSoft_projct/src/iCraft_muleSoft_projct1/Sample1.java
@@ -7,6 +7,33 @@ public class Sample1 {
 
 System.out.println("The theory of hungry bird is very heartbroken.");
 System.out.println("Hello world!");
-	}
+System.out.println("create table students1(\r\n" + 
+		"ID int primary key auto_increment,\r\n" + 
+		" first_name varchar(30)not null,\r\n" + 
+		" last_name varchar(30)not null,\r\n" + 
+		" address varchar(59)not null,\r\n" + 
+		" postal_code int,\r\n" + 
+		" city varchar(30)not null,\r\n" + 
+		" email varchar(40)not null,\r\n" + 
+		" state varchar(20) not null\r\n" + 
+		");\r\n" + 
+		"-- drop table students1;\r\n" + 
+		"-- alter table students1\r\n" + 
+		"-- add primary key(ID);\r\n" + 
+		"-- rename table students1 to infostudents;\r\n" + 
+		"-- alter table infostudents\r\n" + 
+		"-- add age int not null;\r\n" + 
+		"-- alter table infostudents\r\n" + 
+		"-- add country varchar(50) not null;\r\n" + 
+		"-- alter table infostudents\r\n" + 
+		"-- add check(age>5);\r\n" + 
+		"-- Insert into infostudents values\r\n" + 
+		"-- (1, 'Abebe', 'Kebede', '43213', '43526', 'Columbus', 'abejdjd@gmail.com', 'OH',43, 'Moanata' ),\r\n" + 
+		"-- (2, 'Ayana', 'Belay', '43222', '57685', 'Denver', 'cmdjaj@yahoo.com', 'NY', 86, 'kenya'),\r\n" + 
+		"-- (3, 'bana', 'kelay', '43262', '57785', 'Cenver', 'mdjxj@yahoo.com', 'NW', 23, 'Wleya'),\r\n" + 
+		"-- (4, 'babqa', 'keley', '44354', '57674', 'Anana', 'kmgjxj@yahoo.com', 'HU', 46, 'amaya')\r\n" 
 
+		);
+
+}
 }
