7fb863788ba64b70acc85f79ee50bd3edaf4b3e5
Mytirsit <tirsit.mabera@gmail.com>
2022-07-15 22:25:43 -0400

    Finally we did it.

---
+++ wowproject/src/wowproject/Greatpro.java
@@ -0,0 +1,11 @@
+package wowproject;
+
+public class Greatpro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I have good classemats");
+
+	}
+
+}
