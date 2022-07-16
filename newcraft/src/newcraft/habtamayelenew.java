dc927fddfe3e7888164ab443661af3f2cdd505a8
HabtamuAyele <habtamuayele2015@gmail.com>
2022-07-16 03:43:58 -0700

    commit

---
+++ newcraft/src/newcraft/habtamayelenew.java
@@ -0,0 +1,13 @@
+package newcraft;
+
+public class habtamayelenew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("printsomething");
+		System.out.println("printsomething");
+		System.out.println("printsomething");
+		System.out.println("printsomething");
+	}
+
+}
