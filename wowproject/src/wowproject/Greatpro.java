18250872a42593b477cce37d35908567789dc222
Mytirsit <tirsit.mabera@gmail.com>
2022-07-15 22:41:46 -0400

    Second trial

--- wowproject/src/wowproject/Greatpro.java
+++ wowproject/src/wowproject/Greatpro.java
@@ -1,11 +1,11 @@
 package wowproject;
 
-public class Greatpro {
+public class Greatpro { 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("I have good classemats");
-
+System.out.println("I am glade having you.");
 	}
 
 }
