cccd4f28b5779fb89ad4785608487bfd2eb10700
HabtamuAyele <habtamuayele2015@gmail.com>
2022-07-16 18:55:50 -0700

    commit from eclipse

---
+++ eclipse/src/eclipse/eclipsehabtamuayele.java
@@ -0,0 +1,11 @@
+package eclipse;
+
+public class eclipsehabtamuayele {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!");
+        System.out.print("Welcome to www.tutorialkart.com.");
+	}
+
+}
