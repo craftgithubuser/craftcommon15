86df340d745e98dcb77a4ad28f5676a633651d44
Twordofa <tizitabebe12@gmail.com>
2022-07-19 18:11:57 -0700

    quiz1 added

---
+++ Tizita_db_quiz1/mysql.sql
@@ -0,0 +1,15 @@
+SELECT * FROM studentinfo.classes;
+
+create database MyDB;
+
+ create table customers (
+ customerID int auto_increment not null,
+ customerFirstName varchar (100) not null,
+ customerLastName varchar (50) not null,
+ customerNickName varchar (50),
+ customerAge int,
+ customerGender char not null,
+ customerCountry varchar (50) not null,
+ customerSalary dec not null,
+ primary key (customerID)
+);
