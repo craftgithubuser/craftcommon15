9880be3ed1451dffff56b3a523cf0fe6146a47a7
Yakisoemky <108762810+Yakisoemky@users.noreply.github.com>
2022-07-19 21:20:07 -0400

    Add files via upload

---
+++ quiz.sql
@@ -0,0 +1,21 @@
+create schema StudnetsInfo;
+use StudentsInfo;
+
+create table Students(
+StudentId int,
+StduentName varchar(255),
+ParentName varchar(25),
+Address varchar(25),
+PostalCode int,Students
+city varchar(255));
+
+create database school_db;
+use school_db;
+
+
+create table courses(
+course_id int,
+title varchar(100),
+number_of_credit int,
+course_code varchar(20)
+);
