e41b01105282906898aa2724cd42edb6ca546e3e
natidires <108776574+natidires@users.noreply.github.com>
2022-07-21 14:41:45 -0400

    db quiz has been done

---
+++ Nathnael_DB-Quiz 1.sql
@@ -0,0 +1,29 @@
+
+
+-- create database named nate_db_quiz 
+create database nate_db_quiz
+-- create database named studentsinfo
+create database StudentsInfo;
+-- creating table 
+use StudentsInfo;
+
+create table studentsInfo(
+StudentID int not null,
+FirstName varchar(255) not null,
+LastName varchar(255) not null ,
+Address varchar(255),
+Zipcode int not null,
+City varchar(255),
+Country varchar(255) not null,
+Fee int not null
+);
+
+
+
+
+
+
+ 
+
+
+ 
