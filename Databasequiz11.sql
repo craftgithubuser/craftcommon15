bf7e2af80e7071e9703266e3d783ec4004775d78
EmebetH <eminatye@gmail.com>
2022-07-21 00:21:46 -0400

    I submit DML commands quiz

--- Databasequiz11.sql
+++ Databasequiz11.sql
@@ -10,7 +10,7 @@ Cust_lastName char(30) NOT NULL,
 cust_NickName char(20),
 cust_age int,
 Cust_Gender char(10) NOT NULL,
-Cust_country varchar(20) NOT NULL,
+Cust_country varchar(20) default 'Ethiopia',
 Cust_salary decimal NOT NULL
 );
 -- modifying table
@@ -18,4 +18,40 @@ alter table customers
 add cust_Nickname char(10) after cust_lastName;
 alter table customers
 add cust_age int after cust_Nickname;
+alter table customers
+add primary key(cust_ID);
+alter table customers
+modify cust_ID int not null auto_increment;
+alter table customers
+drop column cust_Nickname;
+-- practising DML commands
+insert into customers
+values(null, "Lina", "Mao", 22, "Female", "Romania", 120000);
+insert into customers
+values(null, "Nick", "Jones", null, "M", "USA", 20000000);
+insert into customers
+values(222, "Antony", "Martial", null, "M", "France", 10000000);
+insert into customers
+values(333, "Ross", "Geller", null, "M", "uzbekistan", 36000000);
+insert into customers
+values(444, "Chandler ", "Bing", null, "M", "Unkown", 3400000);
+insert into customers
+values(555, "Rachel ", "Green", null, "F", "kazakhstan", 400000);
+insert into customers
+values(666, "Nick", "Jones", null, "M", "USA", 20000000);
+insert into customers
+values(777, "Poul", "Pogba", null, "M", "France", 20000000);
+insert into customers
+values(888, "Joey ", "Tribbiani", null, "M", "Ethiopia", 70000000);
 
+-- 2. Updating Cust_ID=333,444,555 to country Ethiopian
+update customers 
+set Cust_country = case Cust_ID
+when 333 then 'Ethiopian'
+when 444 then 'Ethiopian'
+when 555 then 'Ethiopian' 
+else Cust_country
+end
+where Cust_ID IN(333,444,555);
+-- 3.Delete Cust_name=nick Jones whichCust_ID is 666
+delete from customers where cust_firstname ='nick' and Cust_lastName='jones' and cust_ID=666;
