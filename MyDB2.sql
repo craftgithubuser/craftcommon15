b56fd9318601f990578c00ce0d4bb72fcc8b93f5
kalyirga01 <ksiyoum22@gmail.com>
2022-07-21 17:20:48 -0400

    Adding

---
+++ MyDB2.sql
@@ -0,0 +1,13 @@
+
+use mydb;
+
+CREATE table Customers(
+Id int Primary key,
+FirstName varchar(25),
+LastName varchar(25),
+nickName varchar(15),
+Age integer,
+Gender char(10), 
+country Varchar(20),
+salary decimal(65)
+);
