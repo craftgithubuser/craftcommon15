89642a7e12b89c932597a377c36a994c6511cbad
Twordofa <tizitabebe12@gmail.com>
2022-07-21 19:58:24 -0700

    quiz2 is added

---
+++ Tizita_db_quiz1/quiz2.sql
@@ -0,0 +1,37 @@
+
+  -- quiz2
+
+alter table customers
+add bonus int not null;
+
+alter table customers
+modify customerAge int not null;
+
+alter table customers
+drop column customerNickName;
+
+alter table customers
+drop column bonus; 
+
+alter table customers 
+rename to customer;
+
+drop table customer;
+
+drop database mydb;
+
+create database MyDB;
+
+use MyDB;
+
+ create table customers (
+ customerID int auto_increment not null,
+ customerFirstName varchar (100) not null,
+ customerLastName varchar (50) not null,
+ customerNickName varchar (50),
+ customerAge int,
+ customerGender char not null,
+ customerCountry varchar (50) not null,
+ customerSalary dec not null,
+ primary key (customerID)
+);
