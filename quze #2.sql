f1a7f742d105df78dc642a11d62843da25388b04
tagesse <108777535+Tagesse@users.noreply.github.com>
2022-07-22 15:29:27 -0600

    I added new quize #2

---
+++ quze #2.sql
@@ -0,0 +1,35 @@
+create database MyDB;
+use MyDB;
+ create table Custumers(
+Cust_ID int not null,
+Cust_FirstName varchar(50) not null,
+Cust_LastName  varchar(50) not null,
+Cust_NickName varchar(50),
+Cust_Age int,
+Cust_Gender char not null,
+Cust_Country varchar(50) not null,
+Cust_Salary decimal not null
+);
+Alter table Custumers
+Add Bonus int not null;
+alter table custumers
+modify Cust_Age int;
+alter table custumers
+drop Cust_NickName; 
+alter table customers
+drop bonus;
+rename table custumers to customer;
+drop table customer;
+drop schema mydb;
+create schema MYDB;
+USE MYDB;
+create table customers(
+Cust_ID int not null ,
+Cust_FirstName varchar(50) not null,
+Cust_LastName  varchar(50) not null,
+Cust_NickName varchar(50) not null,
+Cust_Age int,
+Cust_Gender char not null,
+Cust_Country varchar(50) not null,
+Cust_Salary decimal not null
+);
