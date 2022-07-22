4f592eab7cc3e84570e2e2336ef8a067a65fd846
ttadess <tsegatadesse07@gmail.com>
2022-07-22 13:30:31 -0700

    i updated my quiz

--- mysql.sql
+++ mysql.sql
@@ -18,6 +18,32 @@ alter table customers
 drop Cust_NickName; 
 alter table customers
 drop bonus;
+rename table customers to customer;
+
+drop table customer;
+drop schema mydb;
+
+create schema MYDB;
+USE MYDB;
+create table customers(
+Cust_ID int not null ,
+Cust_FirstName varchar(50) not null,
+Cust_LastName  varchar(50) not null,
+Cust_NickName varchar(50) not null,
+Cust_Age int,
+Cust_Gender char not null,
+Cust_Country varchar(50) not null,
+Cust_Salary decimal not null
+);
+
+
+
+
+
+
+
+
+
 
 
 
