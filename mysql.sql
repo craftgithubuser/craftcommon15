f26a3fe0231135ed31490655f822b1ad755df709
ttadess <tsegatadesse07@gmail.com>
2022-07-22 12:26:28 -0700

    i did my quiz

---
+++ mysql.sql
@@ -0,0 +1,23 @@
+create Schema mydb;
+use mydb;
+create table customers(
+Cust_ID int not null ,
+Cust_FirstName varchar(50) not null,
+Cust_LastName  varchar(50) not null,
+Cust_NickName varchar(50) not null,
+Cust_Age int,
+Cust_Gender char not null,
+Cust_Country varchar(50) not null,
+Cust_Salary decimal not null
+);
+Alter table Customers
+Add Bonus int not null;
+alter table customers
+modify Cust_Age int;
+alter table customers
+drop Cust_NickName; 
+alter table customers
+drop bonus;
+
+
+
