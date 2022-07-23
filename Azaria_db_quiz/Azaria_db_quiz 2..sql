499885f9f2bf30061d0138c5d9315a639bd8d114
AzariaMeheret <azariameheret@icloud.com>
2022-07-23 09:49:01 -0700

    file added

--- Azaria_db_quiz/Azaria_db_quiz 2..sql
+++ Azaria_db_quiz/Azaria_db_quiz 2..sql
@@ -11,3 +11,35 @@ Cust_Country varchar(30) not null,
 Cust_Salary decimal not null
 
 );
+alter table customers 
+modify Cust_ID int auto_increment
+
+
+
+alter table customers
+drop Cust_Nickname
+
+insert mydb.customers (Cust_ID,Cust_FirstName,Cust_LastName,Cust_Gender,Cust_Country,Cust_Salary)
+value("111", "Nick","Jones", "M","USA",20000000);
+
+insert  mydb.customers(Cust_ID,Cust_FirstName,Cust_LastName,Cust_Gender,Cust_Country,Cust_Salary)
+value(222,"Antony","Martial","M","France",10000000)
+
+insert into mydb.Customers (Cust_ID,Cust_FirstName,Cust_LastName,Cust_Gender,Cust_Country,Cust_Salary)
+Values(333,"Ross","Geller","M","uzbekistan",360000000);
+
+insert into MyDB.Customers (Cust_ID,Cust_FirstName,Cust_LastName,Cust_Gender,Cust_Country,Cust_Salary)
+Values(444,"Chandler","Bing","M","Unkown",34000000);
+
+insert into MyDB.Customers (Cust_ID,Cust_FirstName,Cust_LastName,Cust_Gender,Cust_Country,Cust_Salary)
+Values(555,"Rechel","Green","M","Kazakhstan",400000);
+
+insert into MyDB.Customers (Cust_ID,Cust_FirstName,Cust_LastName,Cust_Gender,Cust_Country,Cust_Salary)
+Values(666,"Nick","Joes","M","USA",20000000);
+
+insert into MyDB.Customers (Cust_ID,Cust_FirstName,Cust_LastName,Cust_Gender,Cust_Country,Cust_Salary)
+Values(777,"Poul","Pogba","M","France",20000000);
+
+insert into MyDB.Customers (Cust_ID,Cust_FirstName,Cust_LastName,Cust_Gender,Cust_Country,Cust_Salary)
+Values(888,"Joey","Tribbiani","M","Ethiopia",70000000);
+
