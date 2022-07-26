f993dd79ede1dcaa801d85836523fd6d907e1d00
ttadess <tsegatadesse07@gmail.com>
2022-07-26 11:06:00 -0700

    i added my DML quiz

--- mysql.sql
+++ mysql.sql
@@ -36,6 +36,51 @@ Cust_Country varchar(50) not null,
 Cust_Salary decimal not null
 );
 
+insert into customers
+values(111, 'Nick', 'Jones', 'j', 23, 'M', 'USA', 20000000),
+(222, 'Anthony', 'Maritial', 'k', 45, 'M', 'France', 10000000),
+(333, 'Ross', 'Geller', 'g', 12, 'M', 'Uzbakistan', 3600000),
+(444, 'Chandler', 'Bing','ch', 50, 'M', 'Unknown', 3400000),
+(555, 'Rachel', 'Green','rg', 32, 'F', 'Kazakhstan', 400000),
+(666, 'Nick', 'Jones', 'j', 25, 'M', 'USA', 20000000),
+( 777, 'Poul', 'Pogba', 'pp', 42, 'M', 'France', 20000000),
+(888, 'Joey', 'Tribiani','tr',  21, 'M', 'Ethiopia', 70000000);
+
+update customers
+SET Cust_Country= 'Ethiopia'
+where Cust_ID=333;
+
+update customers
+SET Cust_Country= "Ethiopia"
+where Cust_ID= 444;
+
+update customers
+SET Cust_Country= 'Ethiopia'
+where Cust_ID= 555;
+update customers
+SET Cust_Country= 'Ethiopia'
+where Cust_ID=333 and 444 and 555;
+
+Delete from customers
+where cust_id= 666;
+
+select cust_id 
+from customers;
+select cust_firstName, Cust_Country
+from customers;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
