30b568267ee6c56d39d4caafa9fc68b5d2472b18
EmebetH <eminatye@gmail.com>
2022-08-02 00:47:58 -0400

    JavaDay4 Added!

---
+++ Day4Emebet/src/com/day4/RelationalOperator.java
@@ -0,0 +1,19 @@
+package com.day4;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b;
+        a=40;
+        b=40;
+        System.out.println("a == b = " + (a == b) ); //true
+        System.out.println("a != b = " + (a != b) ); // false
+        System.out.println("a > b = " + (a > b) );  //fale
+        System.out.println("a < b = " + (a < b) ); //false
+        System.out.println("b >= a = " + (b >= a) ); //true
+        System.out.println("b <= a = " + (b <= a) );    //true
+
+	}
+
+}
