e8eeb73244e7b9db1fb377cc13cc8756b785ca82
tesgiz <tesgiz@hotmail.com>
2022-08-08 23:56:02 -0400

    This is my submission to the Java Challenges
    a little change to file Descending order

---
+++ TesfayeAbriha_Java_Challenges/src/com/JavaChallenges/MinimumEngineer.java
@@ -0,0 +1,43 @@
+package com.JavaChallenges;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinimumEngineer {
+	public static int findMin(int[] myArray, int count) {
+		int a;
+		for (int i = 0; i < count; i++) {
+			for (int j = i + 1; j < count; j++) {
+				if (myArray[i] > myArray[j]) {
+					a = myArray[i];
+					myArray[i] = myArray[j];
+					myArray[j] = a;
+				}
+			}
+		}
+		return myArray[0];
+	}
+
+	static Scanner scanner = new Scanner(System.in);
+
+	public static int[] readIntegers(int count) {
+		System.out.println("Please Enter the size of the array. The size is: ");
+		count = scanner.nextInt();
+
+		int[] myArray = new int[count];
+		for (int i = 0; i < count; i++) {
+			System.out.println("Enter the number in idex " + (i));
+			myArray[i] = scanner.nextInt();
+		}
+		return myArray;
+	}
+
+	public static void main(String args[]) {
+
+		int myArray[] = readIntegers(0);
+		System.out.println("My Numbers in the Array: " + Arrays.toString(myArray));
+		System.out.println("The Minimum Number from the array is : " + findMin(myArray, myArray.length));
+
+	}
+
+}
---
+++ TesfayeAbriha_Java_Challenges/src/com/JavaChallenges/ReverseArrayChallenge.java
@@ -0,0 +1,29 @@
+package com.JavaChallenges;
+
+import java.util.Arrays;
+
+public class ReverseArrayChallenge {
+
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5 };
+		reverse(array);
+	}
+
+	public static void reverse(int[] array) {
+
+		int[] reverseArray = new int[array.length];
+
+		int j = 0;
+
+		for (int i = array.length - 1; i >= 0; i--) {
+			reverseArray[i] = array[j];
+
+			j++;
+		}
+
+		System.out.println("The original array was: " + Arrays.toString(array));
+
+		System.out.println("The reversed array is: " + Arrays.toString(reverseArray));
+
+	}
+}
