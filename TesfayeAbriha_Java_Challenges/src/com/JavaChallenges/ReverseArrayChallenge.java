9f99b2472da1c824c32bfdc30765828f92809ec5
tesgiz <tesgiz@hotmail.com>
2022-08-09 16:02:14 -0400

    the method of input has been updated
    the method of input has been updated. It was originally designed to input the array manually but later updated to take values dynamically by calling the readIntegers method from the MinimumInteger class.

--- TesfayeAbriha_Java_Challenges/src/com/JavaChallenges/ReverseArrayChallenge.java
+++ TesfayeAbriha_Java_Challenges/src/com/JavaChallenges/ReverseArrayChallenge.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class ReverseArrayChallenge {
 
 	public static void main(String[] args) {
-		int[] array = { 1, 2, 3, 4, 5 };
-		reverse(array);
+//		int[] array = { 1, 2, 3, 4, 5 }; // if we need to insert array values
+//		reverse(array);
+		int myArray[] = MinimumInteger.readIntegers(0);//if we want to get the values of the array to dynamically be inserted
+		reverse(myArray);
 	}
 
 	public static void reverse(int[] array) {
