9f46208d0798a1d45be0322d503187f6677ab27b
041384ha <igodahalima@gmail.com>
2022-08-12 00:01:48 -0400

    challenge 2 and 3

---
+++ Halima_Java Challenge/Reverse.java
@@ -0,0 +1,35 @@
+package challenge3;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] array1 =new int[] {1,2,3,4,5,6,7,8};
+		System.out.print("The original array is: ");
+			
+		for(int i=0; i<array1.length; i++) {
+			System.out.print(array1[i]+ "");
+			}
+		System.out.println();
+		System.out.print("The reversed array is: ");
+			
+		for(int i = array1.length-1;i>=0;i--)
+		System.out.print(array1[i]+"");
+		}
+		}
+
+
+
+  
+	   
+	  
+	    	
+	  
+
+	   
+	  
+  
+
+
+ 
