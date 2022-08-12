f61c443055d1b6f00917e1a63acc0b99c18c20c3
Tgemechu03 <gemechuamsalu143@gmail.com>
2022-08-12 09:26:35 -0600

    Merge branch 'GemechuDBQuiz' of https://github.com/craftgithubuser/craftcommon15.git into HEAD

--- HelloClass/src/com/pack1/HelloCraftClass.java
+++ HelloClass/src/com/pack1/HelloCraftClass.java
@@ -4,7 +4,7 @@ package com.pack1;
 
 public class HelloCraftClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //push to GemechuT_Branch
 		// TODO Auto-generated method stub
 
 		System.out.println("Hi all!");
