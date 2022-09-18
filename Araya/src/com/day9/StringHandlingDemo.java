bc724fe4e3030890e1a06fa716282b4058c167a0
Marakiabigail <arayafantaa@gmail.com>
2022-09-18 12:44:10 -0400

    string demo is modified

--- Araya/src/com/day9/StringHandlingDemo.java
+++ Araya/src/com/day9/StringHandlingDemo.java
@@ -1,12 +1,12 @@
-package com.stringhandling.day9;
+package com.day9;
 
 public class StringHandlingDemo {
 public static void main (String[]args) {
 	String name =new String("James");
-	String name "James";
-	System.out.println(name.contact(" Bond")); //James Bond
+	String name1 = "James";
+	System.out.println(name.concat(" Bond")); //James Bond
 	System.out.println(name);
-	String str1="I"+"Like"
+	String str1="I"+"Like";
 	
 }
 }
